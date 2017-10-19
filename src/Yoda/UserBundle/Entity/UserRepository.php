43c9cb0e30597a4cf73d6613935efcfb2955a20e
Christos Paraskeva <chris.paraskeva10@gmail.com>
2017-10-19 13:43:02 +0100

    Users can now log in with either a username or email address

--- src/Yoda/UserBundle/Entity/UserRepository.php
+++ src/Yoda/UserBundle/Entity/UserRepository.php
@@ -3,8 +3,12 @@
 namespace Yoda\UserBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 
-class UserRepository extends \Doctrine\ORM\EntityRepository
+class UserRepository extends EntityRepository implements UserProviderInterface
 {
     public function findOneByUsernameOrEmail($username)
     {
@@ -16,4 +20,45 @@ class UserRepository extends \Doctrine\ORM\EntityRepository
             ->getOneOrNullResult()
             ;
     }
+
+    public function loadUserByUsername($username)
+    {
+//        if ($username == "jarjarbinks") {
+//            //can stop certain types of usernames?
+//            var_dump($username);die;
+//        }
+
+        $user = $this->findOneByUsernameOrEmail($username);
+        if (!$user) {
+            throw new UsernameNotFoundException('No user found for username ' . $username);
+        }
+
+        return $user;
+
+    }
+
+    public function refreshUser(UserInterface $user)
+    {
+        $class = get_class($user);
+        if (!$this->supportsClass($class)) {
+            throw new UnsupportedUserException(sprintf( 'Instances of "%s" are not supported.',
+                $class
+            ));
+        }
+
+        if (!$refreshedUser = $this->find($user->getId())) {
+            throw new UsernameNotFoundException(sprintf('User with id %s not found', json_encode($user->getId())));
+        }
+
+        return $refreshedUser;
+    }
+
+    public function supportsClass($class)
+    {
+        return $this->getEntityName() === $class
+            || is_subclass_of($class, $this->getEntityName());
+    }
+
+    // ... refreshUser and supportsClass from above...
+
 }
