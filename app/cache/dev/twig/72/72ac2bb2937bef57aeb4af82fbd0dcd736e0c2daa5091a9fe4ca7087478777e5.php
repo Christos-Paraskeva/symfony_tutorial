a450432dc15ee63e099a32dd791e5cd507804977
Christos Paraskeva <chris.paraskeva10@gmail.com>
2017-10-27 12:46:44 +0100

    Only allows you to invite a user who is not the same as the current logged in user

--- app/cache/dev/twig/72/72ac2bb2937bef57aeb4af82fbd0dcd736e0c2daa5091a9fe4ca7087478777e5.php
+++ app/cache/dev/twig/72/72ac2bb2937bef57aeb4af82fbd0dcd736e0c2daa5091a9fe4ca7087478777e5.php
@@ -21,20 +21,20 @@ class __TwigTemplate_1e387b71bda3866bb0d0f8021ce5c06990106067edbadf7d4f065440ffc
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_60ee9e0f31de0d8fcdb2abf574489c47ebf0b02cccad738ac80b29089474207e = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
-        $__internal_60ee9e0f31de0d8fcdb2abf574489c47ebf0b02cccad738ac80b29089474207e->enter($__internal_60ee9e0f31de0d8fcdb2abf574489c47ebf0b02cccad738ac80b29089474207e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "EventBundle:Event:show.html.twig"));
+        $__internal_9e6cb5e04b3d2956fa8b2b03b1ee0e8b17bf88f79f7c83047e10bdc1727736f2 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_9e6cb5e04b3d2956fa8b2b03b1ee0e8b17bf88f79f7c83047e10bdc1727736f2->enter($__internal_9e6cb5e04b3d2956fa8b2b03b1ee0e8b17bf88f79f7c83047e10bdc1727736f2_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "EventBundle:Event:show.html.twig"));
 
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
-        $__internal_60ee9e0f31de0d8fcdb2abf574489c47ebf0b02cccad738ac80b29089474207e->leave($__internal_60ee9e0f31de0d8fcdb2abf574489c47ebf0b02cccad738ac80b29089474207e_prof);
+        $__internal_9e6cb5e04b3d2956fa8b2b03b1ee0e8b17bf88f79f7c83047e10bdc1727736f2->leave($__internal_9e6cb5e04b3d2956fa8b2b03b1ee0e8b17bf88f79f7c83047e10bdc1727736f2_prof);
 
     }
 
     // line 3
     public function block_body($context, array $blocks = array())
     {
-        $__internal_fd243b66fffaa2276d47c7ce2be58fc3d047926675e41a6d1ac70595a2e142a5 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
-        $__internal_fd243b66fffaa2276d47c7ce2be58fc3d047926675e41a6d1ac70595a2e142a5->enter($__internal_fd243b66fffaa2276d47c7ce2be58fc3d047926675e41a6d1ac70595a2e142a5_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+        $__internal_457261ec64a7febd4e8fbe284d71eab80081167fb5d8d938bf700a6d2871710c = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_457261ec64a7febd4e8fbe284d71eab80081167fb5d8d938bf700a6d2871710c->enter($__internal_457261ec64a7febd4e8fbe284d71eab80081167fb5d8d938bf700a6d2871710c_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
 
         // line 4
         echo "
@@ -152,24 +152,38 @@ class __TwigTemplate_1e387b71bda3866bb0d0f8021ce5c06990106067edbadf7d4f065440ffc
                             ";
         }
         // line 56
-        echo "                        ";
+        echo "                        <div> Invite </div>
+
+                        ";
+        // line 58
         $context['_parent'] = $context;
         $context['_seq'] = twig_ensure_traversable((isset($context["users"]) ? $context["users"] : $this->getContext($context, "users")));
         foreach ($context['_seq'] as $context["_key"] => $context["user"]) {
-            // line 57
-            echo "                            <a href=\"";
-            echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("event_invite", array("user_id" => $this->getAttribute($context["user"], "id", array()), "event_id" => $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "id", array()))), "html", null, true);
-            echo "\" class=\"btn btn-warning btn-xs\">
-                            <li>";
-            // line 58
-            echo twig_escape_filter($this->env, $this->getAttribute($context["user"], "username", array()), "html", null, true);
-            echo "</li>
-                        ";
+            // line 59
+            echo "                            ";
+            if (($this->getAttribute($context["user"], "id", array()) != $this->getAttribute($this->getAttribute((isset($context["app"]) ? $context["app"] : $this->getContext($context, "app")), "user", array()), "id", array()))) {
+                // line 60
+                echo "                              <a href=\"";
+                echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("event_invite", array("user_id" => $this->getAttribute($context["user"], "id", array()), "event_id" => $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "id", array()))), "html", null, true);
+                echo "\" class=\"btn btn-warning btn-xs\">
+                              <li>";
+                // line 61
+                echo twig_escape_filter($this->env, $this->getAttribute($context["user"], "username", array()), "html", null, true);
+                echo "</li>
+                                  ";
+            }
+            // line 63
+            echo "
+                                    ";
+            // line 65
+            echo "                                ";
+            // line 66
+            echo "                                    ";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['user'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 60
+        // line 67
         echo "
                     </dd>
                 </dl>
@@ -178,7 +192,7 @@ class __TwigTemplate_1e387b71bda3866bb0d0f8021ce5c06990106067edbadf7d4f065440ffc
     </section>
 ";
         
-        $__internal_fd243b66fffaa2276d47c7ce2be58fc3d047926675e41a6d1ac70595a2e142a5->leave($__internal_fd243b66fffaa2276d47c7ce2be58fc3d047926675e41a6d1ac70595a2e142a5_prof);
+        $__internal_457261ec64a7febd4e8fbe284d71eab80081167fb5d8d938bf700a6d2871710c->leave($__internal_457261ec64a7febd4e8fbe284d71eab80081167fb5d8d938bf700a6d2871710c_prof);
 
     }
 
@@ -194,7 +208,7 @@ class __TwigTemplate_1e387b71bda3866bb0d0f8021ce5c06990106067edbadf7d4f065440ffc
 
     public function getDebugInfo()
     {
-        return array (  173 => 60,  165 => 58,  160 => 57,  155 => 56,  147 => 52,  139 => 48,  137 => 47,  134 => 46,  127 => 44,  119 => 42,  114 => 41,  108 => 38,  99 => 34,  93 => 31,  87 => 28,  81 => 25,  75 => 21,  69 => 19,  67 => 18,  62 => 16,  53 => 12,  45 => 7,  40 => 4,  34 => 3,  11 => 1,);
+        return array (  187 => 67,  181 => 66,  179 => 65,  176 => 63,  171 => 61,  166 => 60,  163 => 59,  159 => 58,  155 => 56,  147 => 52,  139 => 48,  137 => 47,  134 => 46,  127 => 44,  119 => 42,  114 => 41,  108 => 38,  99 => 34,  93 => 31,  87 => 28,  81 => 25,  75 => 21,  69 => 19,  67 => 18,  62 => 16,  53 => 12,  45 => 7,  40 => 4,  34 => 3,  11 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -262,10 +276,17 @@ class __TwigTemplate_1e387b71bda3866bb0d0f8021ce5c06990106067edbadf7d4f065440ffc
                                     I totally want to go!
                                 </a>
                             {% endif %}
+                        <div> Invite </div>
+
                         {% for user in users %}
-                            <a href=\"{{ path('event_invite', {'user_id': user.id, 'event_id': entity.id}) }}\" class=\"btn btn-warning btn-xs\">
-                            <li>{{ user.username }}</li>
-                        {% endfor %}
+                            {%  if user.id != app.user.id %}
+                              <a href=\"{{ path('event_invite', {'user_id': user.id, 'event_id': entity.id}) }}\" class=\"btn btn-warning btn-xs\">
+                              <li>{{ user.username }}</li>
+                                  {% endif %}
+
+                                    {#{{ dump(app.user.id) }}#}
+                                {#{{ dump(user.id) }}#}
+                                    {% endfor %}
 
                     </dd>
                 </dl>
